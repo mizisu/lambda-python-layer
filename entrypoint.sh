c26d566fee6b197084135502320e26035b3d47c2
mizisu <alwltn960406@gmail.com>
2020-07-07 23:21:39 +0900

    Added delete cache command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 python3 -m venv venv
 source venv/bin/activate
-pip install -r ./requirements.txt
+pip install --no-cache-dir -r ./requirements.txt
 cp -r ./venv/lib/python3.7/site-packages ./python
+find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
 zip -r py37-lambda-layer.zip ./python
